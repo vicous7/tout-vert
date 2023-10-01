9ab7403c1790e345423a0287f44b6f010607adc5
vicous <vicous@free.fr>
2023-10-01 20:37:37 +0200

    fitst

---
+++ main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import os
+import datetime
+import random
+
+def whatDay(jours_a_remonter):
+    date_actuelle = datetime.date.today()
+
+
+    jours_a_remonter = int(jours_a_remonter)
+
+
+    date_passee = date_actuelle - datetime.timedelta(days=jours_a_remonter)
+
+    nom_jour = date_passee.strftime("%A")
+
+    return nom_jour
+
+def make_commit(daysAgo):
+    # créer un commit dans le répository en changeant légèrement un fichier
+    os.system(f"echo '{daysAgo}' > file")
+    os.system('git add file')
+    os.system(f"git commit -m '{daysAgo}' --date relative:{daysAgo}.days.ago")
+
+def make_commits(days):
+    for i in range(days):
+        if whatDay(days)!= "Saturday" or "Sunday":
+	    random_number = random.randint(1, 4)
+	    for i in range(random_number):
+                make_commit(i)
+	else:
+		make_commit(i)
+
+
+
+
+
+
+make_commits(20)
+
+os.system("git push --force origin main")
+random_number = random.randint(1, 10)
